51c8e542defd09328bb8f3251e7b9c16b9469531
Simon Moe <simmoe@gmail.com>
2019-11-13 08:45:40 +0100

    upd gh-pages

---
+++ src/components/Oppmuntring.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Oppmuntring = (props) => {
+    return (
+        <div class="oppmuntring">
+            <h1>Kom igjen {props.navn}</h1>
+            <p>{props.emne} er ikke så vanskelig som du tror.</p>
+            <p>Du må bare øve</p>
+        </div>
+    )
+}
+
+export default Oppmuntring
