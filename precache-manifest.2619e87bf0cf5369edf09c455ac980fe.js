ddd9def811953a45336e4bc2d7ea1cff173b9a1c
Simon Moe <simmoe@gmail.com>
2019-11-13 08:45:08 +0100

    Updates

---
+++ precache-manifest.2619e87bf0cf5369edf09c455ac980fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a94d00af44a1b2f9be378df94ed7854",
+    "url": "/oppgave3/index.html"
+  },
+  {
+    "revision": "7df5e5ee3a5215059367",
+    "url": "/oppgave3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "586774eea3351b0c066b",
+    "url": "/oppgave3/static/js/2.2db8ca07.chunk.js"
+  },
+  {
+    "revision": "7df5e5ee3a5215059367",
+    "url": "/oppgave3/static/js/main.097e4238.chunk.js"
+  },
+  {
+    "revision": "3e0dc9d1b89a58c9b475",
+    "url": "/oppgave3/static/js/runtime-main.f4448b1d.js"
+  }
+]);
